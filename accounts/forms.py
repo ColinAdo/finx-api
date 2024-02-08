7aa3e2b67be9ed63feba46f05e2f3a3d0eb6346f
ColinAdo <colinadodev30@gmail.com>
2024-02-08 12:04:49 +0300

    Add CustomUser model forms

---
+++ accounts/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + (
+            "email",
+            "header", 
+            "profile_picture",
+            "github", 
+            "instagram", 
+            "x", 
+            "linkedin", 
+            "followers",
+            )
+        
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields
