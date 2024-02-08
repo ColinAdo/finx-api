a03d2092ac20af23405fd17a932fdb77c92e97fb
ColinAdo <colinadodev30@gmail.com>
2024-02-08 12:40:43 +0300

    Add test to following unfollowing activities

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class FollowActivityTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.User = get_user_model()
+        cls.User.objects.create(
+            username="testuser",
+            email="testuser@example.com"
+        )
+
+        cls.User.objects.create(
+            username="test2user",
+            email="test2user@example.com"
+        )
+
+    def test_follow(self):
+        userOne = self.User.objects.get(id=1)
+        userTwo = self.User.objects.get(id=2)
+        userOne.follow(userTwo)
+
+        following = userOne.is_following(userTwo)
+        following_count = userOne.followers.all().count()
+
+
+        self.assertEqual(following_count, 1)
+        self.assertEqual(following, True)
+
+    def test_unfollow(self):
+        userOne = self.User.objects.get(id=1)
+        userTwo = self.User.objects.get(id=2)
+        userOne.follow(userTwo)
+
+        userOne.unfollow(userTwo)
+        following = userOne.followers.all().count()
+        is_following = userOne.is_following(userTwo)
+
+        self.assertEqual(following, 0)
+        self.assertEqual(is_following, False)
+
+    def test_is_follow(self):
+        userOne = self.User.objects.get(id=1)
+        userTwo = self.User.objects.get(id=2)
+        userOne.follow(userTwo)
+
+        following = userOne.is_following(userTwo)
+        self.assertEqual(following, True)
