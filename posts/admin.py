c21913f97ef0374c28b758eb063b769d2718b6cf
ColinAdo <colinadodev30@gmail.com>
2024-02-08 19:08:35 +0300

    Add post model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin 
+from django.utils.html import mark_safe
 
-# Register your models here.
+from .models import Post
+
+class PostAdmin(ModelAdmin):
+    list_display = ["author", "display_file", "caption", "created_at"]
+
+    def display_file(self, obj):
+        if obj.file and obj.get_file_type() == "image":
+            return mark_safe('<a href="{}"> <img src="{}" width="30" height="30" style="border-radius: 50%;" /> </a>'.format(obj.file.url, obj.file.url))
+        elif obj.file and obj.get_file_type() == "video":
+            return mark_safe('<a href="{}"> <video width="30" height="30" style="border-radius: 50%;" src="{}"></video>  </a>'.format(obj.file.url, obj.file.url))
+        else:
+            return "text post"
+    display_file.short_description = 'File'
+
+
+admin.site.register(Post, PostAdmin)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
 
-# Create your models here.
+import magic
+
+class Post(models.Model):
+    uuid = ShortUUIDField(max_length=10, length=10, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    file = models.FileField(upload_to="posts", blank=True)
+    caption = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_file_type(self):
+        file_path = self.file.path
+        mime = magic.from_file(file_path, mime=True)
+
+        if mime.startswith("image"):
+            return "image"
+        elif mime.startswith("video"):
+            return "video"
+        else:
+            return "unknown file type"
+        
+
+    def __str__(self):
+        return self.author.username
+
