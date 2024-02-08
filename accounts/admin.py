28d833e641c7ea7bfc1152683325d1e32e1e5677
ColinAdo <colinadodev30@gmail.com>
2024-02-08 19:08:54 +0300

    Add display profile picture to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from django.utils.html import mark_safe
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
@@ -10,6 +11,7 @@ class CustomUserAdmin(UserAdmin):
     list_display = [
         "username",
         "email",
+        "display_image",
         "is_active",
         "is_superuser",
     ]
@@ -33,6 +35,10 @@ class CustomUserAdmin(UserAdmin):
         "linkedin",
         "followers",
     )}),)
+
+    def display_image(self, obj):
+        return mark_safe('<a href="{}"> <img src="{}" width="30" height="30" style="border-radius: 50%;" /> </a>'.format(obj.profile_picture.url, obj.profile_picture.url))
+    display_image.short_description = 'Image'
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
