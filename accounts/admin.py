eae2ab3e9a119d18e1068baf913d832ee980ef4b
ColinAdo <colinadodev30@gmail.com>
2024-02-08 12:05:32 +0300

    Register CustomUser model to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = [
+        "username",
+        "email",
+        "is_active",
+        "is_superuser",
+    ]
+
+    fieldsets = UserAdmin.fieldsets + ((None, { "fields": (
+        "header",
+        "profile_picture",
+        "github", 
+        "instagram",
+        "x",
+        "linkedin",
+        "followers",
+    )}),)
+
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": (
+        "header",
+        "profile_picture",
+        "github",
+        "instagram",
+        "x",
+        "linkedin",
+        "followers",
+    )}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
