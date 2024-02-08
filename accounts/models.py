b3a688234a92f479891d08ea2f7b0127e182ac90
ColinAdo <colinadodev30@gmail.com>
2024-02-08 12:04:25 +0300

    Add CustomUser mode

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    header = models.CharField(max_length=200, blank=True, null=True)
+    # profession = models.CharField(max_length=200, blank=True, null=True)
+    # location = models.CharField(max_length=200, blank=True, null=True)
+    profile_picture = models.ImageField(max_length=200, default="profile.png" , upload_to='profile')
+    github = models.URLField(max_length=200, blank=True, null=True)
+    instagram = models.URLField(max_length=200, blank=True, null=True)
+    x = models.URLField(max_length=200, blank=True, null=True)
+    linkedin = models.URLField(max_length=200, blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            return self.followers.add(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            return self.followers.remove(user)
+
+    def is_following(self, user):
+        return self.followers.filter(id=user.id).exists()
+    
+    # TODO:
+    # Get post of the user and the users he/she is following
+
