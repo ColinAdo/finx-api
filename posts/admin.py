8b88ec172195c1ddae303d09c6678cade632ca61
ColinAdo <colinadodev30@gmail.com>
2024-02-09 07:26:39 +0300

    Add comment model

--- posts/admin.py
+++ posts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from django.utils.html import mark_safe
 
-from .models import Post, Like
+from .models import Post, Like, Comment
 
 
 class PostAdmin(ModelAdmin):
@@ -22,5 +22,10 @@ class LikeAdmin(ModelAdmin):
     list_display = ["user", "post", "created_at"]
 
 
+class CommentAdmin(ModelAdmin):
+    list_display = ["user", "post", "created_at"]
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Like, LikeAdmin)
+admin.site.register(Comment, CommentAdmin)
--- posts/models.py
+++ posts/models.py
@@ -40,3 +40,13 @@ class Like(models.Model):
 
     def __str__(self):
         return f"{self.user.username} liked the post"
+    
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} commented on this post"
