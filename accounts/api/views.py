4d7de743555182283671c5fd7539a34c34d93610
ColinAdo <colinadodev30@gmail.com>
2024-02-10 11:24:29 +0300

    Add users api

---
+++ accounts/api/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+
+from .serializers import UserSerializer
+
+User = get_user_model()
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
