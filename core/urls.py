df45a420063a93248d7dc05234c965a43baacdd9
ColinAdo <colinadodev30@gmail.com>
2024-02-10 13:31:01 +0300

    Add posts URLConf

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('accounts.api.urls'))
+    path('api/', include('accounts.api.urls')),
+    path('api/', include('posts.api.urls')),
 ]
 
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
