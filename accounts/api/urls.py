10b690aab3e5bb820872c3778a0eb4b06c46e7c9
ColinAdo <colinadodev30@gmail.com>
2024-02-12 18:34:58 +0300

    Add URLConf for user-detail

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -4,5 +4,5 @@ from .views import UserListView, UserApiDetailView
 
 urlpatterns = [
     path("users/", UserListView.as_view(), name="users"),
-    path("users/<int:pk>/", UserApiDetailView.as_view(), name="users"),
+    path("users/<int:pk>/", UserApiDetailView.as_view(), name="user-detail"),
 ]
