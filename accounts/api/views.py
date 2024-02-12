c2e9c522cc8225d9171320f1670a10bdd4542606
ColinAdo <colinadodev30@gmail.com>
2024-02-12 17:39:27 +0300

    Updated UserList to UserListView

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -4,6 +4,6 @@ from rest_framework import generics
 from .serializers import UserSerializer
 
 User = get_user_model()
-class UserList(generics.ListAPIView):
+class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
