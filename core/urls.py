d95b812bfd554f3bd3238d73ad63e06f97b814ce
ColinAdo <colinadodev30@gmail.com>
2024-04-16 19:53:00 +0300

    Add djsoer URLConf

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.jwt')),
     path('api/v1/', include('accounts.api.urls')),
     path('api/v1/', include('posts.api.urls')),
     path('api/v1/', include('chats.api.urls')),
