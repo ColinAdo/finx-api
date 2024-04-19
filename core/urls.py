c95df7a83817da70a02c6f8545203293952a3328
ColinAdo <colinadodev30@gmail.com>
2024-04-19 15:56:32 +0300

    Add overriden post URLConf

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('djoser.urls')),
-    path('api/', include('djoser.urls.jwt')),
+    path('api/', include('accounts.urls')),
     path('api/v1/', include('accounts.api.urls')),
     path('api/v1/', include('posts.api.urls')),
     path('api/v1/', include('chats.api.urls')),
