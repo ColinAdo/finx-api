4a6007f60d0dd7413c8e6c41f7fb7ae6750a38d9
ColinAdo <colinadodev30@gmail.com>
2024-05-04 11:27:17 +0300

    Add readonly owner field

--- profiles/api/serializers.py
+++ profiles/api/serializers.py
@@ -3,6 +3,7 @@ from profiles.models import Profile
 from rest_framework import serializers
 
 class ProfileSerializers(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Profile
         fields = '__all__'
