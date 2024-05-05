1d885c3726bd1e2c400f9bd6c655a3a3b6d80bce
ColinAdo <colinadodev30@gmail.com>
2024-05-05 17:07:04 +0300

    Removed old comment from post

--- posts/admin.py
+++ posts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from django.utils.html import mark_safe
 
-from .models import Post, Comment
+from .models import Post
 
 class PostAdmin(ModelAdmin):
     list_display = ["author", "display_file", "caption", "created_at"]
@@ -21,4 +21,3 @@ class CommentAdmin(ModelAdmin):
     list_display = ["user", "post", "created_at"]
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Comment, CommentAdmin)
--- posts/models.py
+++ posts/models.py
@@ -27,12 +27,3 @@ class Post(models.Model):
     def __str__(self):
         return self.author.username
     
-
-class Comment(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.user.username} commented on this post"
