84274f3cc85f4439a68b748927bf1be5e535c52a
ColinAdo <colinadodev30@gmail.com>
2024-05-06 22:31:37 +0300

    Update chats URLConf

--- core/urls.py
+++ core/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('api/', include('comments.api.urls')),
     path('api/', include('likes.api.urls')),
     path('api/', include('followers.api.urls')),
-    path('api/v1/', include('chats.api.urls')),
+    path('api/', include('chats.api.urls')),
 ]
 
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
