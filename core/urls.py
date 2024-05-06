a128c8305a68f489c6061af949974e5e3e94527f
ColinAdo <colinadodev30@gmail.com>
2024-05-06 22:10:06 +0300

    Add URLConfs

--- core/urls.py
+++ core/urls.py
@@ -7,11 +7,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('djoser.urls')),
     path('api/', include('accounts.urls')),
-    path('api/', include('accounts.api.urls')),
+    path('api/v1/', include('accounts.api.urls')),
     path('api/', include('profiles.api.urls')),
     path('api/', include('posts.api.urls')),
     path('api/', include('comments.api.urls')),
     path('api/', include('likes.api.urls')),
+    path('api/', include('followers.api.urls')),
     path('api/v1/', include('chats.api.urls')),
 ]
 
