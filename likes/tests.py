e8f478d448ba87107ab5c60e9b7b9a1d7b1ac565
ColinAdo <colinadodev30@gmail.com>
2024-05-11 19:19:17 +0300

    Add test for likePost model

--- likes/tests.py
+++ likes/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
-# Create your tests here.
+from accounts.models import CustomUser
+from posts.models import Post
+from likes.models import LikePost
+
+
+class TestLikePost(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = CustomUser.objects.create(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpassword'
+        )
+
+        cls.post = Post.objects.create(
+            author=cls.user,
+            caption='first post',
+        )
+
+        cls.like = LikePost.objects.create(
+            user=cls.user,
+            post=cls.post,
+        )
+
+    def test_like_post(self):
+        self.assertEqual(LikePost.objects.count(), 1)
+        self.assertEqual(self.like.user, self.user)
+        self.assertEqual(self.like.post, self.post)
+        self.assertEqual(str(self.like), f'{self.user.username} likes')
