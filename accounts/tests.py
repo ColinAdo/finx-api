384acebc46f45155855b130fd666d27dd746a0b2
ColinAdo <colinadodev30@gmail.com>
2024-05-11 17:29:41 +0300

    Add test to CustomUser model

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,50 +1,25 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-# Follow and unfollow test cases 
-class FollowActivityTests(TestCase):
+# Test User model
+class UserTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
         cls.User = get_user_model()
         cls.User.objects.create(
             username="testuser",
-            email="testuser@example.com"
+            email="testuser@example.com",
+            password="admin1111"
         )
 
-        cls.User.objects.create(
-            username="test2user",
-            email="test2user@example.com"
-        )
 
-    def test_follow(self):
-        userOne = self.User.objects.get(id=1)
-        userTwo = self.User.objects.get(id=2)
-        userOne.follow(userTwo)
+    def test_user_fields(self):
+        user = self.User.objects.get(id=1)
+        
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "testuser@example.com")
+        self.assertEqual(user.password, "admin1111")
 
-        following = userOne.is_following(userTwo)
-        following_count = userOne.followers.all().count()
-
-
-        self.assertEqual(following_count, 1)
-        self.assertEqual(following, True)
-
-    def test_unfollow(self):
-        userOne = self.User.objects.get(id=1)
-        userTwo = self.User.objects.get(id=2)
-        userOne.follow(userTwo)
-
-        userOne.unfollow(userTwo)
-        following = userOne.followers.all().count()
-        is_following = userOne.is_following(userTwo)
-
-        self.assertEqual(following, 0)
-        self.assertEqual(is_following, False)
-
-    def test_is_follow(self):
-        userOne = self.User.objects.get(id=1)
-        userTwo = self.User.objects.get(id=2)
-        userOne.follow(userTwo)
-
-        following = userOne.is_following(userTwo)
-        self.assertEqual(following, True)
+    def test_resturn_string(self):
+        self.assertEqual(str(self.User.objects.get(id=1)), "testuser")
