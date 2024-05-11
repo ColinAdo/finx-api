89a8d6f4ea37b819bce525733d112328d964616e
ColinAdo <colinadodev30@gmail.com>
2024-05-11 17:51:44 +0300

    Add test retrieve user

--- accounts/api/test_accounts_api.py
+++ accounts/api/test_accounts_api.py
@@ -2,10 +2,11 @@ from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
+from rest_framework_simplejwt.tokens import AccessToken
 
 from accounts.api.serializers import UserSerializer
 
-
+# Test users api
 class UserApiTestCase(APITestCase):
 
     @classmethod
@@ -16,31 +17,31 @@ class UserApiTestCase(APITestCase):
             email="TestUser@example.com",
             password="TestUser",
         )
+        cls.access_token = AccessToken.for_user(cls.user)
 
     def test_get_users(self):
         url = reverse("users")
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
         response = self.client.get(url, format="json")
 
         queryset = self.User.objects.all()
         expected_data = UserSerializer(queryset, many=True).data
 
-        for user_data in expected_data:
-            user_data['profile_picture'] = f"http://testserver{user_data['profile_picture']}"
-
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data, expected_data)
         self.assertContains(response, self.user)
+
+        # for user_data in expected_data:
+        #     user_data['profile_picture'] = f"http://testserver{user_data['profile_picture']}"
         
     def test_retrieve_users(self):
         url = reverse("user-detail", kwargs={"pk": self.user.id})
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
         response = self.client.get(url, format="json")
 
         obj = self.User.objects.get(pk=self.user.id)
         expected_data = UserSerializer(obj).data
-
-        expected_data["profile_picture"] = f"http://testserver{expected_data['profile_picture']}"
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, expected_data)
