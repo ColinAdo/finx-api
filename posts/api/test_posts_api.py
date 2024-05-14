093bb98d856a7b0329efbf987465216e0343762a
ColinAdo <colinadodev30@gmail.com>
2024-05-14 11:40:08 +0300

    Add test to post api endpoints

--- posts/api/test_posts_api.py
+++ posts/api/test_posts_api.py
@@ -2,10 +2,11 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
+from rest_framework_simplejwt.tokens import AccessToken
 
 
-from posts.models import Post, Comment
-from posts.api.serializers import PostSerializer, CommentSerializer
+from posts.models import Post
+from posts.api.serializers import PostSerializer
 
 
 class PostsApiTestCase(APITestCase):
@@ -23,14 +24,10 @@ class PostsApiTestCase(APITestCase):
             author=cls.user,
             caption="This is a test post"
         )
+        cls.access_token = AccessToken.for_user(cls.user)
 
-        cls.comment = Comment.objects.create(
-            user=cls.user,
-            post=cls.post,
-            content="This is a test comment1"
-        )
-
-    def test_post_posts(self):
+    def test_create_posts(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
         url = reverse('posts-list')
         data = {
             "author": self.user.id,
@@ -41,7 +38,8 @@ class PostsApiTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Post.objects.count(), 2)
 
-    def test_get_posts(self):
+    def test_get_all_posts(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
         url = reverse("posts-list")
         response = self.client.get(url, format="json")
 
@@ -53,7 +51,7 @@ class PostsApiTestCase(APITestCase):
         self.assertEqual(len(response.data), 1)
         self.assertContains(response, str(self.user.id))
 
-    def test_retrieve_posts(self):
+    def test_retrieve_post(self):
         url = reverse("posts-detail", kwargs={"pk": self.post.id})
         response = self.client.get(url)
 
@@ -62,45 +60,5 @@ class PostsApiTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, expected_data)
-        self.assertContains(response, "This is a test post")
+        self.assertContains(response, self.post.caption)
 
-    def test_post_posts_comment(self):
-        url = reverse("comments", kwargs={"pk": self.post.id})
-        data = {
-            "user": self.user.id,
-            "post": self.post.id,
-            "content": "This is a test comment"
-        }
-        response = self.client.post(url, data, format="json")
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Comment.objects.count(), 2)
-
-    def test_get_posts_comments(self):
-        url = reverse("comments", kwargs={"pk": self.post.id})
-        response = self.client.get(url, format="json")
-
-        queryset = Comment.objects.all()
-        expected_data = CommentSerializer(queryset, many=True).data
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Comment.objects.count(), 1)
-        self.assertEqual(response.data, expected_data)
-        self.assertEqual(len(response.data), 1)
-        self.assertContains(response, "This is a test comment1")
-
-    def test_retrieve_post_comments(self):
-        url = reverse(
-            "comment-detail", 
-            kwargs={
-                "pk": self.post.id, 
-                "comment_pk": self.comment.id
-                }
-            )
-        response = self.client.get(url, format="json")
-
-        obj = Comment.objects.get(id=self.comment.id)
-        expected_data = CommentSerializer(obj).data
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, expected_data)
