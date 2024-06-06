fe7799e94b591351f510ee447d4f8800215bdded
ColinAdo <colinadodev30@gmail.com>
2024-06-06 23:16:07 +0300

    Add comment to posts api permissions

--- posts/api/permissions.py
+++ posts/api/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-
+# Read only permissions
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
