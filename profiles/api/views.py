f9aaf7f09bb5527fe3f83d4cd3e02d20f0ba1e34
ColinAdo <colinadodev30@gmail.com>
2024-06-17 16:56:50 +0300

    Add user followers and following to profile data

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -6,6 +6,8 @@ from .serializers import ProfileSerializers
 from .permissions import IsOwnerOrReadOnly
 from posts.api.serializers import PostSerializer
 from posts.models import Post
+from followers.models import UserFollow
+from followers.api.serializers import UserFollowSerializer
 
 # Profile viewset
 class ProfileViewset(viewsets.ModelViewSet):
@@ -21,12 +23,22 @@ class ProfileViewset(viewsets.ModelViewSet):
             user = request.user
             posts = Post.objects.filter(author=user)
             profile = Profile.objects.get(id=pk)
+            followers = UserFollow.objects.filter(user=user)
+            following = UserFollow.objects.filter(follows=user)
             
             serialized_post = PostSerializer(posts, many=True)
+            serialized_followers = UserFollowSerializer(followers, many=True)
+            serialized_following = UserFollowSerializer(following, many=True)
             serialized_profile = ProfileSerializers(profile)
+            followers_count = len(serialized_followers.data)
+            following_count = len(serialized_following.data)
 
             data = {
                 "profile": serialized_profile.data,
+                "followers": serialized_followers.data,
+                "followers_count": followers_count,
+                "following": serialized_following.data,
+                "following_count": following_count,
                 "posts": serialized_post.data
             }
         except:
