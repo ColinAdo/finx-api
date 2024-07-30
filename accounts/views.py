2410b4c5af52165761e926502c6146323080e7a4
ColinAdo <colinadodev30@gmail.com>
2024-07-30 08:21:47 +0300

    Add proper import organization

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.conf import settings
+from djoser.social.views import ProviderAuthView
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from djoser.social.views import ProviderAuthView
 from rest_framework_simplejwt.views import  (
     TokenObtainPairView,
     TokenRefreshView,
