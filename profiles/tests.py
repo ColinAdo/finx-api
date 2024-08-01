342fa41e3c2130948653cc99b80673d0eeab7384
ColinAdo <colinadodev30@gmail.com>
2024-08-01 08:27:56 +0300

    Add conventions

--- profiles/tests.py
+++ profiles/tests.py
@@ -4,7 +4,6 @@ from accounts.models import CustomUser
 
 # Profile test case
 class TestProfile(TestCase):
-
     @classmethod
     def setUpTestData(cls):
         cls.user = CustomUser.objects.create(
@@ -23,7 +22,7 @@ class TestProfile(TestCase):
         profile.header = 'Expert software developer'
         profile.save()
 
-        self.assertIsNotNone(profile, "Profile should exist")
+        self.assertIsNotNone(profile, 'Profile should exist')
         self.assertEqual(profile.owner.username, 'testuser')
         self.assertEqual(profile.profile_picture, 'profile.png')
         self.assertEqual(profile.header, 'Expert software developer')
