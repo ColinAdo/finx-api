85c878a0b95eb7ec0d72577f9b431369700dc405
ColinAdo <colinadodev30@gmail.com>
2024-08-02 08:24:43 +0300

    Add proper code conventions

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import (
-    PostviewSet,
-)
+from .views import PostviewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostviewSet, basename='posts')
