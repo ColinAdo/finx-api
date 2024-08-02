fc12a11daca805447fcc80017fd5f1f173f9010f
ColinAdo <colinadodev30@gmail.com>
2024-08-02 08:25:34 +0300

    Add organized code

--- posts/api/test_posts_api.py
+++ posts/api/test_posts_api.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+
 from rest_framework.test import APITestCase
 from rest_framework import status
 from rest_framework_simplejwt.tokens import AccessToken
