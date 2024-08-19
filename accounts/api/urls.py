8f338ccf7df05de0a0c36ca60d8c0b745c304e9a
ColinAdo <colinadodev30@gmail.com>
2024-08-19 20:25:35 +0300

    Add single quote

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import UserListView, UserApiDetailView
 
 urlpatterns = [
-    path("users/", UserListView.as_view(), name="users"),
-    path("users/<int:pk>/", UserApiDetailView.as_view(), name="user-detail"),
+    path('users/', UserListView.as_view(), name='users'),
+    path('users/<int:pk>/', UserApiDetailView.as_view(), name='user-detail'),
 ]
