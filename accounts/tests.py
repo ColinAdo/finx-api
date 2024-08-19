6eb8e6c9a8f7b660ac22b65f68178df052478be1
ColinAdo <colinadodev30@gmail.com>
2024-08-19 20:24:08 +0300

    Add single quote

--- accounts/tests.py
+++ accounts/tests.py
@@ -8,18 +8,18 @@ class UserTests(TestCase):
     def setUpTestData(cls):
         cls.User = get_user_model()
         cls.User.objects.create(
-            username="testuser",
-            email="testuser@example.com",
-            password="admin1111"
+            username='testuser',
+            email='testuser@example.com',
+            password='admin1111'
         )
 
 
     def test_user_fields(self):
         user = self.User.objects.get(id=1)
         
-        self.assertEqual(user.username, "testuser")
-        self.assertEqual(user.email, "testuser@example.com")
-        self.assertEqual(user.password, "admin1111")
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.email, 'testuser@example.com')
+        self.assertEqual(user.password, 'admin1111')
 
     def test_resturn_string(self):
-        self.assertEqual(str(self.User.objects.get(id=1)), "testuser")
+        self.assertEqual(str(self.User.objects.get(id=1)), 'testuser')
