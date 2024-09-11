b521c8b8f018e0bebfc2afaf8bbd09323ca31595
ColinAdo <colinadodev30@gmail.com>
2024-09-11 14:21:47 +0300

    Add v1 to api endpoints

--- core/urls.py
+++ core/urls.py
@@ -6,15 +6,15 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("chat/", include("chats.urls")),
     path('admin/', admin.site.urls),
-    path('api/', include('djoser.urls')),
-    path('api/', include('accounts.urls')),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('accounts.urls')),
     path('api/v1/', include('accounts.api.urls')),
-    path('api/', include('profiles.api.urls')),
-    path('api/', include('posts.api.urls')),
-    path('api/', include('comments.api.urls')),
-    path('api/', include('likes.api.urls')),
-    path('api/', include('social.api.urls')),
-    path('api/', include('chats.api.urls')),
+    path('api/v1/', include('profiles.api.urls')),
+    path('api/v1/', include('posts.api.urls')),
+    path('api/v1/', include('comments.api.urls')),
+    path('api/v1/', include('likes.api.urls')),
+    path('api/v1/', include('social.api.urls')),
+    path('api/v1/', include('chats.api.urls')),
 ]
 
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
