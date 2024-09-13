7b21b30d2ec77cb2fadf0fa70aa346ef3c6b7567
ColinAdo <colinadodev30@gmail.com>
2024-09-13 16:46:38 +0300

    Updated profile to get curret loggedin user's profile

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import ProfileSerializers
 from .permissions import IsOwnerOrReadOnly
@@ -14,41 +16,34 @@ from social.api.serializers import UserFollowSerializer
 
 # Profile viewset
 class ProfileViewset(viewsets.ModelViewSet):
-    
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializers
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    def retrieve(self, request, pk, *args, **kwargs):
-        try:
-            user = request.user
-            posts = Post.objects.filter(author=user)
-            profile = Profile.objects.get(id=pk)
-            following = UserFollow.objects.filter(follows=user)
-            followers = UserFollow.objects.filter(user=user)
-            
-            serialized_post = PostSerializer(posts, many=True)
-            serialized_following = UserFollowSerializer(following, many=True)
-            serialized_followers = UserFollowSerializer(followers, many=True)
-            serialized_profile = ProfileSerializers(profile)
-            following_count = len(serialized_following.data)
-            followers_count = len(serialized_followers.data)
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        profile = Profile.objects.get(owner=request.user)
+        serializer = ProfileSerializers(profile, context={'request': request})
 
-            data = {
-                'profile': serialized_profile.data,
-                'following': serialized_followers.data,
-                'following_count': followers_count,
-                'followers': serialized_following.data,
-                'followers_count': following_count,
-                'posts': serialized_post.data
-            }
-        except:
-            return Response({'Message': 'User profile does not exist...'})
+        posts = Post.objects.filter(author=request.user)
+        following = UserFollow.objects.filter(follows=request.user)
+        followers = UserFollow.objects.filter(user=request.user)
 
+        serialized_post = PostSerializer(posts, many=True)
+        serialized_following = UserFollowSerializer(following, many=True)
+        serialized_followers = UserFollowSerializer(followers, many=True)
+
+        data = {
+            'profile': serializer.data,
+            'posts': serialized_post.data,
+            'following': serialized_followers.data,
+            'following_count': len(serialized_followers.data),
+            'followers': serialized_following.data,
+            'followers_count': len(serialized_following.data),
+        }
 
         return Response(data)
-
 
