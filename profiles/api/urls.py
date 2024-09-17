e883c1115261c1f5bc156f3f281a0e7bc1c13150
ColinAdo <colinadodev30@gmail.com>
2024-09-17 18:42:16 +0300

    Deleted profiles app

--- profiles/api/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import ProfileViewset
-
-routes = DefaultRouter()
-routes.register(r'profile', ProfileViewset)
-urlpatterns = routes.urls
