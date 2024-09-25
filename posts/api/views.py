c99c0a832e4f1e312ff63dcdad0b829a248c5ef8
ColinAdo <colinadodev30@gmail.com>
2024-09-25 10:24:43 +0300

    Add get post by username

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -16,12 +18,16 @@ class PostviewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    @action(detail=True, methods=['get'], url_path='author-posts')
-    def get_author_posts(self, request, pk=None):
-        post = self.get_object()
+    @action(detail=False, methods=['get'], url_path='author-posts/(?P<username>[^/.]+)')
+    def get_author_posts(self, request, username=None):
+        User = get_user_model()
+       
+        try:
+            author = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({"detail": "User not found."}, status=404)
         
-        author_posts = Post.objects.filter(author=post.author).exclude(id=post.id).order_by('-created_at')
+        author_posts = Post.objects.filter(author=author).order_by('-created_at')
         
         serializer = self.get_serializer(author_posts, many=True)
-        
         return Response(serializer.data)
