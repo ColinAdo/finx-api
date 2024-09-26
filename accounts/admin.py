5dc6a44caee2d1a17b791fba90e981c2d2493361
ColinAdo <colinadodev30@gmail.com>
2024-09-26 09:00:22 +0300

    Add display profile pic

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.admin import UserAdmin
+from django.utils.html import mark_safe
 from django.contrib import admin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
@@ -11,9 +12,14 @@ class CustomUserAdmin(UserAdmin):
     list_display = [
         'username',
         'email',
+        'display_file',
         'is_active',
         'is_superuser',
     ]
 
+    def display_file(self, obj):
+        return mark_safe('<a href="{}"> <img src="{}" width="30" height="30" style="border-radius: 50%;" /> </a>'.format(obj.profile_picture.url, obj.profile_picture.url))
+        
+    display_file.short_description = 'File'
 
 admin.site.register(CustomUser, CustomUserAdmin)
