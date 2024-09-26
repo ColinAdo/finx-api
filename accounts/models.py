d1477c8d01fae1df32c8df7e3f7cdd18dc2dea4b
ColinAdo <colinadodev30@gmail.com>
2024-09-26 13:35:00 +0300

    Add charfield to profile_picture

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-def user_directory_path(instance, filename):
-    return 'profile/{0}/{1}'.format(instance.username, filename)
-
 # Custom user model
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=255, unique=True)
     header = models.CharField(max_length=200, blank=True, null=True)
     profession = models.CharField(max_length=200, blank=True, null=True)
-    profile_picture = models.ImageField(max_length=200, default='profile.png', upload_to=user_directory_path)
+    profile_picture = models.URLField(null=True,blank=True)
     github = models.URLField(max_length=200, blank=True, null=True)
     instagram = models.URLField(max_length=200, blank=True, null=True)
     x = models.URLField(max_length=200, blank=True, null=True)
