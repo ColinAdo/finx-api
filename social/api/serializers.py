8c9a27a7c20066485ec38fc23b6c33bf9d8f94ea
ColinAdo <colinadodev30@gmail.com>
2024-09-28 14:17:46 +0300

    Updated followers serializer

--- social/api/serializers.py
+++ social/api/serializers.py
@@ -4,9 +4,25 @@ from social.models import UserFollow
 
 # UserFollow serializer
 class UserFollowSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
-    follows = serializers.PrimaryKeyRelatedField(read_only=True)
+    # user = serializers.PrimaryKeyRelatedField(read_only=True)
+    # follows = serializers.PrimaryKeyRelatedField(read_only=True)
+    user = serializers.SerializerMethodField()
+    follows = serializers.SerializerMethodField()
 
     class Meta:
         model = UserFollow
-        fields = '__all__'
+        fields = fields = ['id', 'user', 'follows'] 
+
+    def get_user(self, obj):
+        return {
+            'id': obj.user.id,
+            'username': obj.user.username,
+            'profile_picture': obj.user.profile_picture if obj.user.profile_picture else None,
+        }
+
+    def get_follows(self, obj):
+        return {
+            'id': obj.follows.id,
+            'username': obj.follows.username,
+            'profile_picture': obj.follows.profile_picture if obj.follows.profile_picture else None,
+        }
