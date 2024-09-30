94395ced54571e9e48a7ec191eb612b7c287de5a
ColinAdo <colinadodev30@gmail.com>
2024-09-30 22:25:02 +0300

    Removed unnecessary comments

--- bookmark/api/serializer.py
+++ bookmark/api/serializer.py
@@ -5,9 +5,6 @@ from bookmark.models import Bookmark
 # Bookmark post serializer
 class BookmarkSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
-    # post = serializers.SerializerMethodField()
-    # user = serializers.PrimaryKeyRelatedField(read_only=True)
-    # post = serializers.PrimaryKeyRelatedField(read_only=True)
     
     class Meta:
         model = Bookmark
@@ -19,10 +16,3 @@ class BookmarkSerializer(serializers.ModelSerializer):
             'username': obj.user.username,
             'profile_picture': obj.user.profile_picture if obj.user.profile_picture else None,
         }
-
-    # def get_post(self, obj):
-    #     return {
-    #         'id': obj.follows.id,
-    #         'username': obj.follows.username,
-    #         'profile_picture': obj.follows.profile_picture if obj.follows.profile_picture else None,
-    #     }
