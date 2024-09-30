53b7224d58ac883e2e6db1d9c67a69d049b752ae
ColinAdo <colinadodev30@gmail.com>
2024-09-30 22:21:08 +0300

    Add bookmark serializers

--- bookmark/api/serializer.py
+++ bookmark/api/serializer.py
@@ -4,9 +4,25 @@ from bookmark.models import Bookmark
 
 # Bookmark post serializer
 class BookmarkSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
-    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    user = serializers.SerializerMethodField()
+    # post = serializers.SerializerMethodField()
+    # user = serializers.PrimaryKeyRelatedField(read_only=True)
+    # post = serializers.PrimaryKeyRelatedField(read_only=True)
     
     class Meta:
         model = Bookmark
-        fields = '__all__'
+        fields = fields = ['id', 'user', 'post'] 
+
+    def get_user(self, obj):
+        return {
+            'id': obj.user.id,
+            'username': obj.user.username,
+            'profile_picture': obj.user.profile_picture if obj.user.profile_picture else None,
+        }
+
+    # def get_post(self, obj):
+    #     return {
+    #         'id': obj.follows.id,
+    #         'username': obj.follows.username,
+    #         'profile_picture': obj.follows.profile_picture if obj.follows.profile_picture else None,
+    #     }
