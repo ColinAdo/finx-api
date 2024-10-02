b605217ca58aa091847ea5f93b92285ba44e3884
ColinAdo <colinadodev30@gmail.com>
2024-10-02 13:34:32 +0300

    Updates asgi file

--- core/asgi.py
+++ core/asgi.py
@@ -1,22 +1,21 @@
-import chats.routing
 import os
-
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+import chats.routing
+from chats.middleware.jwt_auth_middleware import JWTAuthMiddleware  # Import your custom middleware
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 django_asgi_app = get_asgi_application()
 
-
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(chats.routing.websocket_urlpatterns))
-        ),
-    }
-)
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AllowedHostsOriginValidator(
+        JWTAuthMiddleware(  
+            URLRouter(
+                chats.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
