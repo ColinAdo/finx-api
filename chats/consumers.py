da2c4bfe163bf5131798121a31fdd2a1607264ad
ColinAdo <colinadodev30@gmail.com>
2024-10-09 22:45:39 +0300

    Add proper consumer

--- chats/consumers.py
+++ chats/consumers.py
@@ -1,13 +1,66 @@
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+import json
+from accounts.api.serializers import UserSerializer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         user = self.scope.get('user')
-        if not user.is_authenticated: 
+        if not user.is_authenticated:
             self.close()
             return
-        print('Logged in user:', user)
+
+        self.username = user.username
+        async_to_sync(self.channel_layer.group_add)(
+            self.username, self.channel_name
+        )
         self.accept()
+        print(f"{self.username} connected")
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.username, self.channel_name
+        )
+        print(f"{self.username} disconnected")
+
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        data_source = data.get('source')
+
+        print("Received", json.dumps(data, indent=2))
+
+        if data_source == 'update_profile_picture':
+            self.receive_profile_picture(data)
+
+    def receive_profile_picture(self, data):
+        user = self.scope.get('user')
+        filename = data.get('profile_picture')
+        if filename:
+            user.profile_picture = filename
+            user.save()
+
+            # Serialize updated user data
+            serialized = UserSerializer(user)
+
+            # Broadcast updated profile picture to the group
+            self.send_group(self.username, 'update_profile_picture', serialized.data)
+
+    def send_group(self, group, source, data):
+        response = {
+            'type': 'profile_picture_updated',
+            'source': source,
+            'profile_picture': data['profile_picture']
+        }
+        async_to_sync(self.channel_layer.group_send)(
+            group, response
+        )
+
+
+    def profile_picture_updated(self, event):
+        profile_picture_data = event['profile_picture']
+
+        # Send updated profile data to the connected client
+        self.send(text_data=json.dumps({
+            'type': 'profile_picture_updated',
+            'profile_picture': profile_picture_data
+        }))
