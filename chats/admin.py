cfc27e9fd6e5c4beb0444106454a76e8c2793383
ColinAdo <colinadodev30@gmail.com>
2024-10-15 22:19:25 +0300

    Add comment

--- chats/admin.py
+++ chats/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Contact, Message, Chat
 
+# Registering models
 admin.site.register(Contact)
 admin.site.register(Message)
 admin.site.register(Chat)
