8b58b99f4becdc01f4a0a81f456e64b6e93f0684
ColinAdo <colinadodev30@gmail.com>
2024-10-16 22:16:31 +0300

    Removed unwanted import

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,6 @@
 from django.conf import settings
 from django.db import models
 
-import magic
-
 # Post model
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
