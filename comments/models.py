130224ca20897206e71d474ce4e81e73c68bf526
ColinAdo <colinadodev30@gmail.com>
2024-10-16 22:17:49 +0300

    Removed unwanted comment

--- comments/models.py
+++ comments/models.py
@@ -3,18 +3,12 @@ from django.conf import settings
 
 from posts.models import Post
 
-# file directory 
-# def directory_path(instance, filename):
-#     return 'comments/{0}/{1}'.format(instance.owner.username, filename)
-
-
 # Comment model
 class Comment(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     comment = models.CharField(max_length=4000, blank=True, null=True)
     comment_image = models.CharField(max_length=4000, blank=True, null=True)
-    # comment_image = models.ImageField(upload_to=directory_path, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
