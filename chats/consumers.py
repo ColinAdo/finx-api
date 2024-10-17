3595e9c73e66f18a9c1b1250e7c7d67fa959a82a
ColinAdo <colinadodev30@gmail.com>
2024-10-17 22:48:42 +0300

    Add proper format

--- chats/consumers.py
+++ chats/consumers.py
@@ -1,6 +1,7 @@
+import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-import json
+
 from accounts.api.serializers import UserSerializer
 
 class ChatConsumer(WebsocketConsumer):
