f7fef78f8293e2ed74b80a2249fdc1521574aa31
ColinAdo <colinadodev30@gmail.com>
2024-10-17 22:45:15 +0300

    Add proper format

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 
-
 from accounts.api.serializers import UserSerializer
 
 from posts.models import Post
