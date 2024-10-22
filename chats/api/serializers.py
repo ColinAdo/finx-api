a53d88020af071b92496e3128d34746244c89bc9
ColinAdo <colinadodev30@gmail.com>
2024-10-22 10:10:35 +0300

    Add conversation serializers

--- chats/api/serializers.py
+++ chats/api/serializers.py
@@ -1,28 +1,21 @@
 from rest_framework import serializers
 
-from chats.models import Chat, Contact, Message
+from chats.models import Conversation, ConversationMessage
 from accounts.api.serializers import UserSerializer
 
 #  Contact serialiser class 
-class ContactSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+class ConversationSerializer(serializers.ModelSerializer):
+    users = UserSerializer(many=True, read_only=True)
 
     class Meta:
-        model = Contact
-        fields = '__all__'
+        model = Conversation
+        fields = ('id', 'users', 'modified_at',)
 
 #  Message serialiser class 
-class MessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Message
-        fields = '__all__'
-
-
-#  Chat serialiser class 
-class ChatSerializer(serializers.ModelSerializer):
-    participants = ContactSerializer(many=True, read_only=True)
-    messages = MessageSerializer(many=True, read_only=True)
+class ConversationMessageSerializer(serializers.ModelSerializer):
+    sent_to = UserSerializer(many=False, read_only=True)
+    created_by = UserSerializer(many=False, read_only=True)
 
     class Meta:
-        model = Chat
-        fields = '__all__'
+        model = ConversationMessage
+        fields = ('id', 'body', 'sent_to', 'created_by',)
