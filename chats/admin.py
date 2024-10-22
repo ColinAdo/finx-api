c71b48bb545952e4ed58984fe33e3e790c994983
ColinAdo <colinadodev30@gmail.com>
2024-10-22 10:11:07 +0300

    Add conversation models to admin

--- chats/admin.py
+++ chats/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import Contact, Message, Chat
+from .models import Conversation, ConversationMessage
 
 # Registering models
-admin.site.register(Contact)
-admin.site.register(Message)
-admin.site.register(Chat)
+admin.site.register(Conversation)
+admin.site.register(ConversationMessage)
