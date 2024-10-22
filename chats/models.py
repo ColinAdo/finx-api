ce60ed6d23057f24d03edf7f9d7db02ec952fcb6
ColinAdo <colinadodev30@gmail.com>
2024-10-22 10:10:00 +0300

    Add conversation and conversation messages

--- chats/models.py
+++ chats/models.py
@@ -1,6 +1,22 @@
 from django.conf import settings
 from django.db import models
 
+import uuid
+
+class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+class ConversationMessage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
+    body = models.TextField()
+    sent_to = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='received_messages', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_messages', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
 # contact model
 class Contact(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
