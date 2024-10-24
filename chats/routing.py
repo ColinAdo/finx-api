1b99ef60f11641761f3817e728d2788ddff27a05
ColinAdo <colinadodev30@gmail.com>
2024-10-24 08:20:00 +0300

    Add working chat route

--- chats/routing.py
+++ chats/routing.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path("api/v1/chat/", consumers.ThumbnailConsumer.as_asgi()),
+    path('api/v1/chat/', consumers.ThumbnailConsumer.as_asgi()),
+    path('api/v1/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
 ]
