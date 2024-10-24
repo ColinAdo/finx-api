b7be5ebc1e15a707e02c83ca8ca1d8e8211611c5
ColinAdo <colinadodev30@gmail.com>
2024-10-24 08:52:06 +0300

    Add proper like serializer

--- likes/api/serializers.py
+++ likes/api/serializers.py
@@ -4,9 +4,15 @@ from likes.models import LikePost
 
 # Like post serializer
 class LikeSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
-    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    user = serializers.SerializerMethodField()
     
     class Meta:
         model = LikePost
-        fields = '__all__'
+        fields = fields = ['id', 'user', 'post'] 
+
+    def get_user(self, obj):
+        return {
+            'id': obj.user.id,
+            'username': obj.user.username,
+            'profile_picture': obj.user.profile_picture if obj.user.profile_picture else None,
+        }
