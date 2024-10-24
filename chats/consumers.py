bb14ac979f3c2332720f436205a234e9d84d3889
ColinAdo <colinadodev30@gmail.com>
2024-10-24 08:19:38 +0300

    Add working chat consumer

--- chats/consumers.py
+++ chats/consumers.py
@@ -1,7 +1,10 @@
 import json
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import sync_to_async
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+
 from accounts.api.serializers import UserSerializer
+from .models import ConversationMessage
 
 # Thumbnail consumer
 class ThumbnailConsumer(WebsocketConsumer):
@@ -61,3 +64,64 @@ class ThumbnailConsumer(WebsocketConsumer):
             'type': 'profile_picture_updated',
             'profile_picture': profile_picture_data,
         }))
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+
+        # Join room
+        print(self.room_group_name)
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self):
+        # Leave room
+
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Recieve message from web sockets
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        print("Received", json.dumps(data, indent=2))
+
+        conversation_id = data['data']['conversation_id']
+        sent_to_id = data['data']['sent_to_id']
+        name = data['data']['name']
+        body = data['data']['body']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'body': body,
+                'name': name
+            }
+        )
+        user = self.scope['user']
+
+        await self.save_message(conversation_id, body, sent_to_id)
+    
+    # Sending messages
+    async def chat_message(self, event):
+        body = event['body']
+        name = event['name']
+
+        await self.send(text_data=json.dumps({
+            'body': body,
+            'name': name
+        }))
+
+    @sync_to_async
+    def save_message(self, conversation_id, body, sent_to_id):
+        user = self.scope['user']
+
+        ConversationMessage.objects.create(conversation_id=conversation_id, body=body, sent_to_id=sent_to_id, created_by=user)
