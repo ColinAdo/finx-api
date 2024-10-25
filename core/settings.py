817357c09e1f2695060aecd0c8b33771e6bad563
ColinAdo <colinadodev30@gmail.com>
2024-10-25 16:41:45 +0300

    Updated settings file

--- core/settings.py
+++ core/settings.py
@@ -169,7 +169,7 @@ DJOSER = {
 # cors header settings
 CORS_ALLOWED_ORIGINS = os.getenv(
     'CORS_ALLOWED_ORIGINS', 
-    'http://localhost:3000, http://127.0.0.1:3000'
+    'http://localhost:3000, http://127.0.0.1:3000, https://finx-api-00ec7aa68e8b.herokuapp.com'
 ).split(',')
 
 CORS_ALLOW_CREDENTIALS = True
