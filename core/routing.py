15228ff144d24777d9f76b17c1d48febb8cda116
ColinAdo <colinadodev30@gmail.com>
2024-10-26 09:48:55 +0300

    Add dephne http

--- core/routing.py
+++ core/routing.py
@@ -1,11 +1,15 @@
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
 from chats.middleware.jwt_auth_middleware import JWTAuthMiddleware
 import chats.routing
 
+django_asgi_app = get_asgi_application()
+
 
 application = ProtocolTypeRouter({
+    'http': django_asgi_app,
     'websocket': AllowedHostsOriginValidator(
         JWTAuthMiddleware(  
             URLRouter(
