afc74ce6c365cbc8577a1676be46f3e0e8bb963f
ColinAdo <colinadodev30@gmail.com>
2024-10-26 09:04:19 +0300

    Updated settings

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '.herokuapp.com, 127.0.0.1,localhost').split(',')
 
 
 # Application definition
