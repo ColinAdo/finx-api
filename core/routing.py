078a77420673fe3032347a90d660dbc2eafda2b9
ColinAdo <colinadodev30@gmail.com>
2024-10-28 15:30:45 +0300

    Update code

--- core/routing.py
+++ core/routing.py
@@ -1,13 +1,11 @@
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from channels.routing import get_default_application
 
 from chats.middleware.jwt_auth_middleware import JWTAuthMiddleware
 from chats import routing
 
-# django_asgi_app = get_asgi_application()
-django_asgi_app = get_default_application()
+django_asgi_app = get_asgi_application()
 
 
 application = ProtocolTypeRouter({
