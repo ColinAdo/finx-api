52e1e7284e8da4e5ac52b3794427e6dd198dd753
ColinAdo <colinadodev30@gmail.com>
2024-10-28 16:21:07 +0300

    Update proper code

--- core/asgi.py
+++ core/asgi.py
@@ -1,14 +1,12 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
+
 django_asgi_app = get_asgi_application()
 
 from chats.middleware.jwt_auth_middleware import JWTAuthMiddleware
@@ -16,10 +14,7 @@ from chats.consumers import ThumbnailConsumer, ChatConsumer
 
 
 application = ProtocolTypeRouter({
-    # Django's ASGI application to handle traditional HTTP requests
     "http": django_asgi_app,
-
-    # WebSocket chat handler
     "websocket": AllowedHostsOriginValidator(
         JWTAuthMiddleware(
             URLRouter([
--- core/settings.py
+++ core/settings.py
@@ -120,8 +120,6 @@ TEMPLATES = [
     },
 ]
 
-# WSGI_APPLICATION = 'core.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -261,8 +259,6 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATIC_ROOT = os.path.join(BASE_DIR,  'staticfiles')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
