29462a432b26c0abac0245c1160b3f94461fce91
ColinAdo <colinadodev30@gmail.com>
2024-10-30 12:55:12 +0300

    Add cors header

--- core/settings.py
+++ core/settings.py
@@ -171,7 +171,7 @@ DJOSER = {
 # cors header settings
 CORS_ALLOWED_ORIGINS = os.getenv(
     'CORS_ALLOWED_ORIGINS', 
-    'http://localhost:3000, http://127.0.0.1:3000, https://finx-api-00ec7aa68e8b.herokuapp.com, https://finx-9rny760fj-colinados-projects.vercel.app'
+    'https://finx-9rny760fj-colinados-projects.vercel.app,http://localhost:3000,http://127.0.0.1:3000'
 ).split(',')
 
 CORS_ALLOW_CREDENTIALS = True
