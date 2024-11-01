e3ac48385c075d0bbf815bf29cc19269fba601e1
ColinAdo <colinadodev30@gmail.com>
2024-11-01 20:05:23 +0300

    Removed unwanted models

--- chats/models.py
+++ chats/models.py
@@ -20,27 +20,3 @@ class ConversationMessage(models.Model):
     def __str__(self):
         return f"{self.created_by} => {self.sent_to}"
     
-# contact model
-class Contact(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    friends = models.ManyToManyField('self', blank=True)
-
-    def __str__(self):
-        return self.user.username
-    
-# Message model
-class Message(models.Model):
-    contact = models.ForeignKey(Contact, related_name="messages", on_delete=models.CASCADE)
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.contact.user.username
-    
-# Chat model
-class Chat(models.Model):
-    participants = models.ManyToManyField(Contact, related_name="chats")
-    messages = models.ManyToManyField(Message, blank=True)
-    
-    def __str__(self):
-        return f"Chat - {self.pk}"
